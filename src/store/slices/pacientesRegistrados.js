d8f1d4dfb398b2b5bc7992386da3df28894aa467
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-05-03 15:52:56 -0300

    remove los pacientes de ejemplo

--- src/store/slices/pacientesRegistrados.js
+++ src/store/slices/pacientesRegistrados.js
@@ -8,49 +8,6 @@ const { v4: uuidv4 } = require("uuid");
  * * fechaNac: DD-MM-YYYY
  */
 
-const pacientes = [
-    {
-        nombre: "JUAN GONZALO",
-        apellido: "WILLNECKER",
-        fechaNac: "09-12-1995"
-    },
-    {
-        nombre: "JUAN GONZALO",
-        apellido: "GUTIERREZ",
-        fechaNac: "09-12-1999"
-    },
-    {
-        nombre: "JUAN GONZALO",
-        apellido: "LOPEZ CRUZ",
-        fechaNac: "09-12-1980"
-    },
-    {
-        nombre: "JUAN GONZALO",
-        apellido: "LOPEZ",
-        fechaNac: "09-12-1998"
-    },
-    {
-        nombre: "JUAN GONZALO",
-        apellido: "SOSA",
-        fechaNac: "09-12-1990"
-    },
-    {
-        nombre: "JUAN GONZALO",
-        apellido: "WILLNECKER",
-        fechaNac: "09-12-1996"
-    },
-    {
-        nombre: "JUAN GONZALO",
-        apellido: "WILLNECKER",
-        fechaNac: "09-12-1996"
-    },
-    {
-        nombre: "JUAN GONZALO",
-        apellido: "WILLNECKER",
-        fechaNac: "09-12-1994"
-    },
-]
-
 const reducers = {
     registrar: (state, action) => {
         state.push({ id: uuidv4(), ...action.payload })
